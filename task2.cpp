88f4ebb5665caefa2e5633412c7b392c44a42c56
Pradip <pradip88dhadbale@gmail.com>
2024-04-07 23:53:03 -0700

    task2.cpp
    arithmetic calculator.

---
+++ task2.cpp
@@ -0,0 +1,40 @@
+/*Develop a calculator program that performs basic arithmetic operations such as addition, subtraction, multiplication, and division. 
+  Allow the user to input two numbers and choose an operation to perform.*/
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    double num1, num2;
+    char operation;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    cout << "Choose operation (+, -, *, /): ";
+    cin >> operation;
+    switch (operation) {
+        case '+':
+            cout << "Result: " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << "Result: " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << "Result: " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                cout << "Result: " << num1 / num2 << endl;
+            } else {
+                cout << "Error: Division by zero is not allowed." << endl;
+            }
+            break;
+        default:
+            cout << "Error: Invalid operation." << endl;
+            break;
+    }
+    return 0;
+}
